dce22f284d5335842dac3dac45fa3f0965391f9a
jaca-pereira <jaca.pereira@campus.fct.unl.pt>
2024-05-29 16:29:05 +0100

    added msvd_eval to token_merging

--- scripts/video/eval/msvd_eval.sh
+++ scripts/video/eval/msvd_eval.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7'
+eval "$(conda shell.bash hook)"
+
+conda activate llamavid
+
+echo $CONDA_DEFAULT_ENV
+
+cd ~/Desktop/LLaMA-VID
+
+CUDA_VISIBLE_DEVICES='0,1'
 gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
 IFS=',' read -ra GPULIST <<< "$gpu_list"
 
 CHUNKS=${#GPULIST[@]}
 
 CKPT="llama-vid/llama-vid-7b-full-224-video-fps-1"
-OPENAIKEY=""
-OPENAIBASE=""
+#OPENAIKEY=""
+#OPENAIBASE=""
 
 for IDX in $(seq 0 $((CHUNKS-1))); do
     CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python llamavid/eval/model_msvd_qa.py \
@@ -24,11 +32,11 @@ done
 
 wait
 
-python llamavid/eval/eval_msvd_qa.py \
-    --pred_path ./work_dirs/eval_msvd/$CKPT \
-    --output_dir ./work_dirs/eval_msvd/$CKPT/results \
-    --output_json ./work_dirs/eval_msvd/$CKPT/results.json \
-    --num_chunks $CHUNKS \
-    --num_tasks 16 \
-    --api_key $OPENAIKEY \
-    --api_base $OPENAIBASE
+#python llamavid/eval/eval_msvd_qa.py \
+#   --pred_path ./work_dirs/eval_msvd/$CKPT \
+#    --output_dir ./work_dirs/eval_msvd/$CKPT/results \
+#    --output_json ./work_dirs/eval_msvd/$CKPT/results.json \
+#    --num_chunks $CHUNKS \
+#    --num_tasks 16 \
+#    --api_key $OPENAIKEY \
+#    --api_base $OPENAIBASE
