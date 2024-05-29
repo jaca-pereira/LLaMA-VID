4294903e1d41a8c842c2ed6c6c5da66672468159
jaca-pereira <jaca.pereira@campus.fct.unl.pt>
2024-05-29 21:03:44 +0100

    fix msvd_eval

--- scripts/video/eval/msvd_eval.sh
+++ scripts/video/eval/msvd_eval.sh
@@ -15,8 +15,8 @@ IFS=',' read -ra GPULIST <<< "$gpu_list"
 CHUNKS=${#GPULIST[@]}
 
 CKPT="llama-vid-7b-full-224-video-fps-1"
-#OPENAIKEY=""
-#OPENAIBASE=""
+OPENAIKEY=""
+OPENAIBASE=""
 
 for IDX in $(seq 0 $((CHUNKS-1))); do
     CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python llamavid/eval/model_msvd_qa.py \
@@ -32,11 +32,11 @@ done
 
 wait
 
-#python llamavid/eval/eval_msvd_qa.py \
-#   --pred_path ./work_dirs/eval_msvd/$CKPT \
-#    --output_dir ./work_dirs/eval_msvd/$CKPT/results \
-#    --output_json ./work_dirs/eval_msvd/$CKPT/results.json \
-#    --num_chunks $CHUNKS \
-#    --num_tasks 16 \
-#    --api_key $OPENAIKEY \
-#    --api_base $OPENAIBASE
+python llamavid/eval/eval_msvd_qa.py \
+   --pred_path ./work_dirs/eval_msvd/$CKPT \
+    --output_dir ./work_dirs/eval_msvd/$CKPT/results \
+    --output_json ./work_dirs/eval_msvd/$CKPT/results.json \
+    --num_chunks $CHUNKS \
+    --num_tasks 16 \
+    --api_key $OPENAIKEY \
+    --api_base $OPENAIBASE
