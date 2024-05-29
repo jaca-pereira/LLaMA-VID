5e9e0e0584da57de789e0b711eae1e74a08f9c4d
jaca-pereira <jaca.pereira@campus.fct.unl.pt>
2024-05-29 16:28:05 +0100

    fixed main again and added msvd_eval

--- scripts/video/train/stage_1_full_v7b_224_fps_1.sh
+++ scripts/video/train/stage_1_full_v7b_224_fps_1.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-deepspeed llamavid/train/train_mem.py \
+eval "$(conda shell.bash hook)"
+
+conda activate llamavid
+
+echo $CONDA_DEFAULT_ENV
+
+cd ~/data/LLaMA-VID
+
+wandb login a5569ff69ae6ef0b1d94c04c83e390de9c453efd
+
+deepspeed --module llamavid.train.train_mem \
     --deepspeed ./scripts/zero2.json \
     --model_name_or_path model_zoo/LLM/vicuna/7B-V1.5 \
     --version plain_guided \
-    --data_path ./data/LLaMA-VID-Pretrain/llava_video/llava_558k_with_webvid.json \
-    --image_folder ./data/LLaMA-VID-Pretrain/images \
-    --video_folder ./data/LLaMA-VID-Pretrain/videos \
+    --data_path ./data/LLaMA-VID-Pretrain/subsample_llava_with_webvid.json \
+    --image_folder ./data/LLaMA-VID-Pretrain/ \
+    --video_folder ./data/LLaMA-VID-Pretrain/ \
     --vision_tower ./model_zoo/LAVIS/eva_vit_g.pth \
     --image_processor ./llamavid/processor/clip-patch14-224 \
     --mm_projector_type mlp2x_gelu \
@@ -15,19 +25,15 @@ deepspeed llamavid/train/train_mem.py \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --video_fps 1 \
-    --bert_type "qformer_pretrain_freeze" \
-    --num_query 32 \
-    --pretrain_qformer model_zoo/LAVIS/instruct_blip_vicuna7b_trimmed.pth \
-    --compress_type "mean" \
     --bf16 True \
     --output_dir ./work_dirs/llama-vid-7b-pretrain-224-video-fps-1 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 32 \
-    --per_device_eval_batch_size 4 \
+    --per_device_eval_batch_size 32 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 1000 \
+    --save_steps 500 \
     --save_total_limit 1 \
     --learning_rate 1e-3 \
     --weight_decay 0. \
