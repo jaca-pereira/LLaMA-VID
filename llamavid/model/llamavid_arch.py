fc2ad6bba754b986ea3bd136885316fe7ef0bb51
jaca.pereira <jaca.pereira@campus.fct.unl.pt>
2024-06-20 17:38:21 +0100

    changed half to torch.float 16

--- llamavid/model/llamavid_arch.py
+++ llamavid/model/llamavid_arch.py
@@ -179,7 +179,7 @@ class LLaMAVIDMetaForCausalLM(ABC):
                 text_inputs = self.get_model().extract_nouns_adjectives_verbs(self.get_model().nlp, prompts[i][0])
                 text_inputs = self.get_model().text_tokenizer(text_inputs, return_tensors="pt", truncation=True, padding=True).to(device=self.device)
                 text_embeds = self.get_model().text_model(**text_inputs).last_hidden_state
-                text_embeds = text_embeds.half()
+                text_embeds = text_embeds.type(torch.float16)
                 num_non_image_tokens = (input_ids[i] != IMAGE_TOKEN_INDEX).sum()
                 if getattr(self.config, 'tune_mm_mlp_adapter', False) and getattr(self.config, 'mm_use_im_start_end', False):
                     topk = self.config.max_length - num_non_image_tokens - 1
